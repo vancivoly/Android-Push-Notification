cb828d4ba893121f3823fa3f9e2e9125943fc108
vancivoly <vancivoly@hotmail.com>
2014-03-07 13:00:22 -0800

    Update ReceiveMessage.java

--- ReceiveMessage.java
+++ ReceiveMessage.java
@@ -16,37 +16,34 @@ public class ReceiveMessage extends GCMBaseIntentService{
 
 	@Override
 	protected void onMessage(Context context, Intent intent) {
-        // Please customize!
 
-		// This is how to get values from the push message (data)
-        String message = intent.getStringExtra("message");
-        //long timestamp = intent.getLongExtra("timestamp", -1);
-        String msgType = intent.getStringExtra("msgType");
+		// Get values from the push message
+        	String message = intent.getStringExtra("message");
+        	String msgType = intent.getStringExtra("msgType");
 
-	    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-	    Notification notification = new Notification(R.drawable.ic_launcher, "Push", System.currentTimeMillis());
-	   
-	    RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification);
-	    contentView.setImageViewResource(R.id.imageView1, R.drawable.ic_launcher);
-        contentView.setTextViewText(R.id.notification_title, "Push");
-        contentView.setTextViewText(R.id.notification_text, message);
-        String currentTime = DateFormat.getTimeInstance().format(Calendar.getInstance().getTime());
-        contentView.setTextViewText(R.id.notification_time, currentTime);
-        notification.contentView = contentView;
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notification = new Notification(R.drawable.ic_launcher, "Push", System.currentTimeMillis());
+		   
+		RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification);
+		contentView.setImageViewResource(R.id.imageView1, R.drawable.ic_launcher);
+        	contentView.setTextViewText(R.id.notification_title, "Push");
+        	contentView.setTextViewText(R.id.notification_text, message);
+        	String currentTime = DateFormat.getTimeInstance().format(Calendar.getInstance().getTime());
+        	contentView.setTextViewText(R.id.notification_time, currentTime);
+        	notification.contentView = contentView;
         
-	    Intent notificationIntent = new Intent(context, MainActivity.class);
-
-	    notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-	           Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	    PendingIntent pendingIntent = PendingIntent.getActivity(context, count, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-	    notification.contentIntent = pendingIntent;
-	    notification.setLatestEventInfo(context, "Push", message, pendingIntent);
-	    notification.number = count++;
-	    notification.defaults |= Notification.DEFAULT_SOUND;
-	    notification.defaults |= Notification.DEFAULT_VIBRATE;
-	    notification.defaults |= Notification.DEFAULT_LIGHTS;
-	    notification.flags |= Notification.FLAG_AUTO_CANCEL;
-	    notificationManager.notify(count, notification);
+	    	Intent notificationIntent = new Intent(context, MainActivity.class);
+	    	notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	    	
+	    	PendingIntent pendingIntent = PendingIntent.getActivity(context, count, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+	    	notification.contentIntent = pendingIntent;
+	    	notification.setLatestEventInfo(context, "Push", message, pendingIntent);
+	    	notification.number = count++;
+	    	notification.defaults |= Notification.DEFAULT_SOUND;
+	    	notification.defaults |= Notification.DEFAULT_VIBRATE;
+	    	notification.defaults |= Notification.DEFAULT_LIGHTS;
+	    	notification.flags |= Notification.FLAG_AUTO_CANCEL;
+	    	notificationManager.notify(count, notification);
 			
 	}
 
